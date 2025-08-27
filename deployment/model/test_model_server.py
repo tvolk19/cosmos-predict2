2fc969a147f31bb0501fe5a93ef16118be2a4721
Thomas Volk <tvolk@nvidia.com>
2025-08-27 21:09:26 +0000

    initial fixes

---
+++ deployment/model/test_model_server.py
@@ -0,0 +1,50 @@
+import os
+from deployment.model.video2world_worker import Video2World_Validator
+from deployment.model.text2image_worker import Text2Image_Validator
+from imaginaire.utils import log
+from cosmos_gradio.deployment_env import DeploymentEnv
+from cosmos_gradio.model_ipc.model_server import ModelServer
+
+prompt = "A nighttime city bus terminal gradually shifts from stillness to subtle movement. At first, multiple double-decker buses are parked under the glow of overhead lights, with a central bus labeled '87D' facing forward and stationary. As the video progresses, the bus in the middle moves ahead slowly, its headlights brightening the surrounding area and casting reflections onto adjacent vehicles. The motion creates space in the lineup, signaling activity within the otherwise quiet station. It then comes to a smooth stop, resuming its position in line. Overhead signage in Chinese characters remains illuminated, enhancing the vibrant, urban night scene."
+sample = {"input_path": "assets/video2world/input0.jpg", "num_conditional_frames": 1, "prompt": prompt}
+
+
+def test_text2image():
+    os.environ["FACTORY_MODULE"] = "deployment.model.text2image_worker"
+    cfg = DeploymentEnv()
+
+    folder = "outputs/"
+    with ModelServer(
+        num_gpus=cfg.num_gpus, factory_module=cfg.factory_module, factory_function=cfg.factory_function
+    ) as pipeline:
+        validator = Text2Image_Validator()
+
+        log.info("Inference start****************************************")
+
+        model_params = validator.parse_and_validate({})
+        model_params["output_dir"] = f"{folder}/"
+        pipeline.infer(model_params)
+        log.info("Inference complete****************************************")
+
+
+def test_video2world():
+    os.environ["FACTORY_MODULE"] = "deployment.model.video2world_worker"
+    cfg = DeploymentEnv()
+
+    folder = "outputs/"
+    with ModelServer(
+        num_gpus=cfg.num_gpus, factory_module=cfg.factory_module, factory_function=cfg.factory_function
+    ) as pipeline:
+        validator = Video2World_Validator()
+
+        log.info("Inference start****************************************")
+
+        model_params = validator.parse_and_validate(sample)
+        model_params["output_dir"] = f"{folder}/"
+        pipeline.infer(model_params)
+        log.info("Inference complete****************************************")
+
+
+if __name__ == "__main__":
+    test_text2image()
+    test_video2world()
--- deployment/model/text2image_worker.py
+++ deployment/model/text2image_worker.py
@@ -29,7 +29,8 @@ from cosmos_predict2.configs.base.config_text2image import (
     _PREDICT2_TEXT2IMAGE_PIPELINE_14B,
 )
 from cosmos_predict2.pipelines.text2image import Text2ImagePipeline
-from deployment.server.model_specific.model_config import Config
+from deployment.model.model_config import Config
+from cosmos_gradio.deployment_env import DeploymentEnv
 from imaginaire.utils import distributed, log
 from imaginaire.utils.io import save_image_or_video, save_text_prompts
 
@@ -180,11 +181,12 @@ class Text2Image_Worker:
 def create_worker(create_model=True):
     log.info("Creating predict pipeline and validator")
     cfg = Config()
+    global_env = DeploymentEnv()
     pipeline = None
     if create_model:
         pipeline = Text2Image_Worker(
-            num_gpus=int(os.environ.get("WORLD_SIZE", 1)),
-            checkpoint_dir=cfg.checkpoint_dir,
+            num_gpus=global_env.num_gpus,
+            checkpoint_dir=global_env.checkpoint_dir,
             model_size=cfg.model_size,
             load_ema=cfg.load_ema,
         )
